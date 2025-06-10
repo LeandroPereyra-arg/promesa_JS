ed5c7283fd5143697e2b129cfe3d45ce77a3be1a
LeandroPereyra-arg <leanpereyra.48@gmail.com>
2025-06-09 21:32:28 -0300

    Primer commit

---
+++ Java Script/arrays.js
@@ -0,0 +1,18 @@
+export const usuarios=[
+    {
+    usuario: 'Pablo',
+    password: '1234',
+    tipo: 'admin'
+    },
+    {
+    usuario: 'Leandro',
+    password: '2222',
+    tipo: 'genial'
+    },
+    {
+    usuario: 'santi',
+    password: '3333',
+    tipo: 'genio'
+    },
+
+]
